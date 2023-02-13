470744d0a81568b8ada42818f307689d03cecf44
Vlad Osmianski <vlaosm@gmail.com>
2023-02-13 21:34:59 +0200

    `EditableContent` component

---
+++ resources/js/Components/EditableContent.vue
@@ -0,0 +1,53 @@
+<script setup>
+
+import {nextTick, ref} from "vue";
+
+const props = defineProps({
+    element: { type: String, default: 'div' },
+    modelValue: String,
+});
+const emit = defineEmits(['update:modelValue']);
+
+let oldValue = null;
+const editable = ref(false);
+
+async function edit(e) {
+    if (editable.value) {
+        return;
+    }
+
+    oldValue = props.modelValue;
+    editable.value = true;
+    await nextTick();
+    e.currentTarget.focus();
+}
+
+function commit(e) {
+    if (!editable.value) {
+        return;
+    }
+
+    editable.value = false;
+    oldValue = null;
+    emit('update:modelValue', e.currentTarget.innerText);
+}
+
+function cancel(e) {
+    if (!editable.value) {
+        return;
+    }
+
+    editable.value = false;
+    e.currentTarget.innerText = oldValue;
+    oldValue = null;
+}
+
+</script>
+
+<template>
+    <component :is="element" @click="edit" @blur="commit"
+        @keydown.enter.prevent="commit" @keydown.esc.prevent="cancel" :contenteditable="editable"
+    >
+        {{ modelValue }}
+    </component>
+</template>
