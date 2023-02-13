36ca3a18e3955fa348bb87a16a7dacfa8c1190f7
Vlad Osmianski <vlaosm@gmail.com>
2023-02-13 16:12:18 +0200

    Installed IDE helper

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -5,6 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Page
+ *
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $title
+ * @property string|null $body
+ * @method static \Illuminate\Database\Eloquent\Builder|Page newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Page newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Page query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Page whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Page whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Page whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Page whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Page whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Page extends Model
 {
     use HasFactory;
--- app/Models/Slug.php
+++ app/Models/Slug.php
@@ -5,6 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Slug
+ *
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string $slug
+ * @property string $controller_class
+ * @property int|null $page_id
+ * @method static \Illuminate\Database\Eloquent\Builder|Slug newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Slug newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Slug query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Slug whereControllerClass($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Slug whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Slug whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Slug wherePageId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Slug whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Slug whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Slug extends Model
 {
     use HasFactory;
