fb2d369c60e850e5bb382da6b7333acef1f5d551
Vlad Osmianski <vlaosm@gmail.com>
2023-02-14 21:18:53 +0200

    Rename `EditableContent` component back to `Editable`

--- resources/js/Pages/Page.vue
+++ resources/js/Pages/Page.vue
@@ -2,7 +2,7 @@
 import { Head } from '@inertiajs/inertia-vue3';
 import { marked } from 'marked';
 import DOMPurify from 'dompurify';
-import EditableContent from "@/Components/EditableContent.vue";
+import Editable from "@/Components/Editable.vue";
 import axios from "axios";
 
 const props = defineProps({
@@ -37,9 +37,10 @@ function saveTitle() {
         <main class="min-w-0 max-w-2xl flex-auto px-4 py-16 lg:max-w-none lg:pr-0 lg:pl-8 xl:px-16">
             <article>
                 <header class="mb-9 space-y-1">
-                    <EditableContent v-model="page.title" @update:model-value="saveTitle"
+                    <Editable v-model="page.title" @update:model-value="saveTitle"
                         element="h1" class="font-display text-3xl tracking-tight text-slate-900 dark:text-white" />
                 </header>
+
                 <div v-html="DOMPurify.sanitize(marked(page.body))" class="prose prose-slate max-w-none dark:prose-invert dark:text-slate-400 prose-headings:scroll-mt-28 prose-headings:font-display prose-headings:font-normal lg:prose-headings:scroll-mt-[8.5rem] prose-lead:text-slate-500 dark:prose-lead:text-slate-400 prose-a:font-semibold dark:prose-a:text-sky-400 prose-a:no-underline prose-a:shadow-[inset_0_-2px_0_0_var(--tw-prose-background,#fff),inset_0_calc(-1*(var(--tw-prose-underline-size,4px)+2px))_0_0_var(--tw-prose-underline,theme(colors.sky.300))] hover:prose-a:[--tw-prose-underline-size:6px] dark:[--tw-prose-background:theme(colors.slate.900)] dark:prose-a:shadow-[inset_0_calc(-1*var(--tw-prose-underline-size,2px))_0_0_var(--tw-prose-underline,theme(colors.sky.800))] dark:hover:prose-a:[--tw-prose-underline-size:6px] prose-pre:rounded-xl prose-pre:bg-slate-900 prose-pre:shadow-lg dark:prose-pre:bg-slate-800/60 dark:prose-pre:shadow-none dark:prose-pre:ring-1 dark:prose-pre:ring-slate-300/10 dark:prose-hr:border-slate-800">
                 </div>
             </article>
