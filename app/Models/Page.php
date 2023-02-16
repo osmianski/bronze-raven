3a08f37b969956af79891985a88afd8105231d12
Vlad Osmianski <vlaosm@gmail.com>
2023-02-16 08:04:59 +0200

    installed Pusher and Laravel Echo

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\PageSaved;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -39,6 +40,10 @@ class Page extends Model
         'owner_id',
         'title',
         'body',
+    ];
+
+    protected $dispatchesEvents = [
+        'saved' => PageSaved::class,
     ];
 
     public function slug(): BelongsTo
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,15 @@
- import '../css/app.css';
+import '../css/app.css';
+import './bootstrap';
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
- import { createApp, h } from 'vue';
- import { createInertiaApp } from '@inertiajs/vue3';
- import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-
- createInertiaApp({
-     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`,
-         import.meta.glob('./Pages/**/*.vue')),
-     setup({ el, App, props, plugin }) {
-         createApp({ render: () => h(App, props) })
-             .use(plugin)
-             .mount(el)
-     },
- })
+createInertiaApp({
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`,
+        import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .mount(el)
+    },
+})
