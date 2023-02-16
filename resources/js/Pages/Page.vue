09b968120f9f54185608e82a1270ae181bbebdf9
Vlad Osmianski <vlaosm@gmail.com>
2023-02-16 07:36:57 +0200

    Migrated to Inertia.js 1.0

--- resources/js/Pages/Page.vue
+++ resources/js/Pages/Page.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { Head } from '@inertiajs/inertia-vue3';
+import { Head } from '@inertiajs/vue3';
 import Editable from "@/Components/Editable.vue";
 import axios from "axios";
 import EditableMarkdown from "@/Components/EditableMarkdown.vue";
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
  import '../css/app.css';
 
  import { createApp, h } from 'vue';
- import { createInertiaApp } from '@inertiajs/inertia-vue3';
+ import { createInertiaApp } from '@inertiajs/vue3';
  import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
  createInertiaApp({
